4036ccc3a96a067061b7a9103517b7f3eb9823e6
lephitar <philippe@phitar.com>
2021-05-10 20:52:13 +0200

    removed turbolinks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
+// Preloader js
+$(window).on('load', function () {
+	$('.preloader').fadeOut(100);
+});
 
-$(document).on('turbolinks:load', function () {
+$(document).ready(function () {
 	'use strict';
-
-	// prelaoder
-	$('.preloader').delay(100).fadeOut(10);
-
-	// re initialize bootstrap dropdown for turbolinks
-	$('.dropdown-toggle').dropdown()
 
 	//slider
 	$('.slider').not('.slick-initialized').slick({
@@ -17,4 +15,4 @@ $(document).on('turbolinks:load', function () {
 		arrows: false
 	});
 
-})(jQuery);
+});
